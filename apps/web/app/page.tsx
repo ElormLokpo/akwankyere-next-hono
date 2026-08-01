2421b148e7feb73b2adb42fc757958d1fe49cbaa
ElormLokpo <benedictdev31@gmail.com>
2026-08-01 22:19:28 +0000

    landing hero

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,102 +1,78 @@
-import Image, { type ImageProps } from "next/image";
-import { Button } from "@repo/ui/button";
-import styles from "./page.module.css";
-
-type Props = Omit<ImageProps, "src"> & {
-  srcLight: string;
-  srcDark: string;
-};
-
-const ThemeImage = (props: Props) => {
-  const { srcLight, srcDark, ...rest } = props;
-
-  return (
-    <>
-      <Image {...rest} src={srcLight} className="imgLight" />
-      <Image {...rest} src={srcDark} className="imgDark" />
-    </>
-  );
-};
+import Image from "next/image";
+import { HiOutlineUser } from "react-icons/hi2";
+import { LuArrowRight } from "react-icons/lu";
+import { RiParentLine } from "react-icons/ri";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <ThemeImage
-          className={styles.logo}
-          srcLight="turborepo-dark.svg"
-          srcDark="turborepo-light.svg"
-          alt="Turborepo logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>apps/web/app/page.tsx</code>
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="bg-background h-screen  " >
+      <div className="sticky top-0 z-50 py-5 px-5 flex items-center justify-center">
+        <div className="bg-white w-150 p-3 rounded-3xl flex items-center justify-between gap-3">
+          <div className="flex items-center  gap-3">
+            <div className="rounded-full"><Image className="rounded-full" src="/logo.png" alt="logo" width={35} height={35} /></div>
+            <div className="text-primary-green-500 text-sm md:text-md font-bold">Akwankyere</div>
+          </div>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new/clone?demo-description=Learn+to+implement+a+monorepo+with+a+two+Next.js+sites+that+has+installed+three+local+packages.&demo-image=%2F%2Fimages.ctfassets.net%2Fe5382hct74si%2F4K8ZISWAzJ8X1504ca0zmC%2F0b21a1c6246add355e55816278ef54bc%2FBasic.png&demo-title=Monorepo+with+Turborepo&demo-url=https%3A%2F%2Fexamples-basic-web.vercel.sh%2F&from=templates&project-name=Monorepo+with+Turborepo&repository-name=monorepo-turborepo&repository-url=https%3A%2F%2Fgithub.com%2Fvercel%2Fturborepo%2Ftree%2Fmain%2Fexamples%2Fbasic&root-directory=apps%2Fdocs&skippable-integrations=1&teamSlug=vercel&utm_source=create-turbo"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://turborepo.dev/docs?utm_source"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+          <div className="flex text-xs md:text-sm gap-2 text-primary-green-500">
+            <div className="hover:cursor-pointer hover:font-bold transition-all">How it works</div>
+            <div className="hover:cursor-pointer hover:font-bold transition-all">About Us</div>
+          </div>
+
         </div>
-        <Button appName="web" className={styles.secondary}>
-          Open alert
-        </Button>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com/templates?search=turborepo&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://turborepo.dev?utm_source=create-turbo"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to turborepo.dev →
-        </a>
-      </footer>
+      </div>
+
+      <div className="px-8 md:px-20 lg:grid lg:grid-cols-2 lg:gap-5 lg:px-35 2xl:px-55 py-10">
+        <div className="text-primary-green-500 lg:px-30 ">
+          <div className="font-semibold text-center lg:text-left">Ghana&lsquo;s career pathway guide</div>
+          <div className="text-[2rem] text-center lg:text-left leading-10 lg:text-[3rem] 2xl:text-[4rem] mb-3 lg:leading-13  2xl:leading-18 font-bold font-serif">Find the school path that fits your future.</div>
+
+          <div className="mb-5 text-center lg:text-left">
+            Discover SHS programme and tertiary courses built around your interests, strengths and results.
+          </div>
+
+          <div className="mb-5 lg:hidden">
+            <Image src="/landing.png" alt="landing" width={1000} height={1000} />
+
+          </div>
+
+          <div>
+          <button className="bg-primary-green-500 mb-2 md:mb-4 px-3 py-3 text-white md:px-5 md:py-3 rounded-2xl w-full flex items-center justify-between transition-transform duration-300 ease-out hover:-translate-y-3">
+              <div className="flex items-center gap-4">
+
+                <div className="bg-background p-2 md:p-4 font-semibold rounded-full text-primary-green-500 flex items-center justify-center">
+                  <HiOutlineUser />
+                </div>
+
+                <div className=" text-sm md:text-md">I&lsquo;m a student</div>
+              </div>
+
+              <div>
+                <LuArrowRight className="mr-2" />
+              </div>
+            </button>
+
+            <button className="bg-gold md:mb-4 text-white p-3 md:px-5 md:py-3 rounded-2xl w-full flex items-center justify-between transition-transform duration-300 ease-out hover:-translate-y-3">
+              <div className="flex items-center gap-4">
+
+                <div className="bg-background p-2 md:p-4 font-semibold rounded-full text-primary-green-500 flex items-center justify-center">
+                  <RiParentLine />
+                </div>
+
+                <div className="text-primary-green-500 text-sm md:text-md font-semibold">I&lsquo;m a parent or gaurdian</div>
+              </div>
+
+              <div>
+                <LuArrowRight className="mr-2" />
+              </div>
+            </button>
+          </div>
+
+        </div>
+        <div className="hidden lg:block col-span-1">
+          <Image src="/landing.png" alt="landing" width={1000} height={1000} />
+        </div>
+      </div>
+
     </div>
   );
 }
